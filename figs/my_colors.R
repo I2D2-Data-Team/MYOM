4d8090cab082fef38d53e5a20a2b28e405075745
Giorgi Chighladze <giorgichi@gmail.com>
2022-10-13 17:01:01 -0500

    Update color palettes

--- figs/my_colors.R
+++ figs/my_colors.R
@@ -4,26 +4,28 @@ library(unikn)
 # Set of color palettes
 color_palette <-
   list(
+    yellow_blu = c(RColorBrewer::brewer.pal(11, "Spectral")[6:11], "darkslateblue"),
     yellow_grn = c("#fff999", "#f1d581", "#b6c45c", "#7db257", "#4c9c53", "#34834b", "#146c37"),
     yellow_red = RColorBrewer::brewer.pal(7, "YlOrRd"),
-    purple_red = RColorBrewer::brewer.pal(7, "RdPu"),
+    # yellow_pur = viridis::magma(18)[seq(17, 5, -2)],
+    yellow_pur = viridis::magma(20)[c(20, seq(17, 6, -2))],
+    red_purple = RColorBrewer::brewer.pal(7, "RdPu"),
     blu_purple = RColorBrewer::brewer.pal(7, "BuPu"),
-    yellow_blu = c(RColorBrewer::brewer.pal(11, "Spectral")[6:11], "darkslateblue"),
-    yellow_pur = viridis::magma(18)[seq(17, 5, -2)],
     viridius = viridis::viridis(13)[seq(13, 1, -2)],
     i2d2 = c("#252C6A", "#0097CE", "#66c0e1", "#76777A", "#c299cc", "#863399", "#CF202E")
   )
 
 # Names of color palettes
 col_names <- 
-  tibble("yellow_grn" = "Yellow to Green",
-         "yellow_red" = "Yellow to Red",
-         "purple_red" = "Purple to Red",
-         "blu_purple" = "Blue to Purple",
-         "yellow_blu" = "Yellow to Blue",
-         "yellow_pur" = "Yellow to Purple",
-         "viridius" = "Viridius Colors",
-         "i2d2" = "I2D2")
+  tibble(
+    "yellow_blu" = "Yellow to Blue",
+    "yellow_grn" = "Yellow to Green",
+    "yellow_red" = "Yellow to Red",
+    "yellow_pur" = "Yellow to Purple",
+    "blu_purple" = "Blue to Purple",
+    "red_purple" = "Red to Purple",
+    "viridius" = "Viridius",
+    "i2d2" = "I2D2")
 
 # Plot color palettes
 png("figs/color_palette.png", width = 600, height = 800, res = 96)
